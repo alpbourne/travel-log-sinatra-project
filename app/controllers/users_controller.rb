1346d4fcfde430649ae42db817ee617074411169
alpbourne <alpbourne@gmail.com>
2017-11-28 04:32:13 +0000

    get /users/:id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
 
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    if !logged_in?
+      redirect '/destinations'
+    end
+    if !@user=="" && @user == current_user
+      erb :'users/show'
+    else
+      redirect '/destinations'
+    end
+  end
 end
