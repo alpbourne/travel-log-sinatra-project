8f10d6212ce2b38c98bfc74d0da9710e02fd777a
alpbourne <alpbourne@gmail.com>
2017-11-28 14:46:49 +0000

    get /destinations

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,10 @@
 class DestinationsController < ApplicationController
+
+  get "/destinations" do
+    redirect_if_not_logged_in
+    @destinations = Destination.all
+    erb :'destinations/index'
+  end
+
+  
 end
