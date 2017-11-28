8d2328531cce4aebbe349849a7f6f452aedc23ef
alpbourne <alpbourne@gmail.com>
2017-11-28 16:00:44 +0000

    get and post /activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,17 @@
 class ActivitiesController < ApplicationController
+
+  get "/activities" do
+    redirect_if_not_logged_in
+    @activities = Activity.all
+    erb :'activities/index'
+  end
+
+  post "/activities" do
+    redirect_if_not_logged_in
+    unless Activity.valid_params?(params)
+      redirect "/activities/new?error=invalid activity"
+    end
+    Activity.create(params)
+    redirect "/activities"
+  end
 end
