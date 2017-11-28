af9234dacad5b3ae74ea125e188ff3d8dd7d8e52
alpbourne <alpbourne@gmail.com>
2017-11-28 04:26:42 +0000

    added helpers to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,27 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "secret"
+    set :session_secret, "letsadventurearound"
   end
 
   get "/" do
     erb :index
+  end
+
+  helpers do
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect "/login?error=You must be logged in for that permission"
+      end
+    end
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
