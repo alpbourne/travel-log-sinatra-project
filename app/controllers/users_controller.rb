6b1258a027789f2c96561f087b612386486bb27d
alpbourne <alpbourne@gmail.com>
2017-12-01 20:10:24 +0000

    changed destination names to URLs in destinations/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
       redirect to "/register"
     else
       @user = User.create(:username => params[:username], :password => params[:password])
+      @user.save
       session[:user_id] = @user.id
       redirect '/destinations'
     end
